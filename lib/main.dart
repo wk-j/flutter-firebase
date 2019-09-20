14552458a8603de6f2629968386dcb96ef3ba247
wk-j <somnuk.wk@gmail.com>
2019-09-20 12:36:06 +0700

    Switch to cloud_firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -22,29 +21,39 @@ class Api {
   //   return await _auth.signInAnonymously();
   // }
 
-  void go() async {
-    var options = const FirebaseOptions(
-      apiKey: "AIzaSyDXyczRuaZWskx43U-Pamzyw1YaUag79lY",
-      // authDomain: "github-issue-bot.firebaseapp.com",
-      databaseURL: "https://github-issue-bot.firebaseio.com",
-      // projectId: "github-issue-bot",
-      storageBucket: "github-issue-bot.appspot.com",
-      // messagingSenderId: "699789446244",
-      googleAppID: "1:699789446244:web:7b2fc9b70a7c64c4986371",
-    );
+  // void go() async {
+  //   var options = const FirebaseOptions(
+  //     apiKey: "AIzaSyDXyczRuaZWskx43U-Pamzyw1YaUag79lY",
+  //     // authDomain: "github-issue-bot.firebaseapp.com",
+  //     databaseURL: "https://github-issue-bot.firebaseio.com",
+  //     // projectId: "github-issue-bot",
+  //     storageBucket: "github-issue-bot.appspot.com",
+  //     // messagingSenderId: "699789446244",
+  //     googleAppID: "1:699789446244:web:7b2fc9b70a7c64c4986371",
+  //   );
 
-    var app =
-        await FirebaseApp.configure(name: "github-issue-bot", options: options);
-    var db = FirebaseDatabase(app: app);
-    db.reference().child("issues").onValue.listen((v) {
-      debugPrint("$v");
-    });
-  }
+  //   var app =
+  //       await FirebaseApp.configure(name: "github-issue-bot", options: options);
+  //   var db = FirebaseDatabase(app: app);
+  //   db.reference().child("issues").onValue.listen((v) {
+  //     debugPrint("$v");
+  //   });
+  // }
+  void go() {}
 }
 
 class MyApp extends StatelessWidget {
+  Widget buildItem(DocumentSnapshot document) {
+    return ListTile(
+      title: Text(document["title"]),
+      subtitle: Text(document["label"]),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    // var stream = Firestore.instance.collection("issues").snapshots();
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -64,15 +73,22 @@ class MyApp extends StatelessWidget {
             },
           ),
         ),
-        body: ListView(
-          children: <Widget>[
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text("Hello"),
-              subtitle: Text("enhancement"),
-              trailing: Icon(Icons.add_comment),
-            )
-          ],
+        body: StreamBuilder(
+          stream: Firestore.instance.collection("issues").snapshots(),
+          builder: (context, shot) {
+            if (!shot.hasData) {
+              return Center(
+                child: Text("Loading ..."),
+              );
+            }
+
+            return ListView.builder(
+              itemExtent: 80.0,
+              itemCount: shot.data.documents.length,
+              itemBuilder: (context, index) =>
+                  buildItem(shot.data.documents[index]),
+            );
+          },
         ),
       ),
     );
